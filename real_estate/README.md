6c521583adb8b4470ac3f20c5497707ed6a3bcab
Vicente Antonio G. Reyes <highcenoid@Highcenburg.local>
2020-04-04 01:38:13 +0800

    push

---
+++ real_estate/README.md
@@ -0,0 +1,1 @@
+# icvn_land
--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'icenreyes@gmail.com'
-EMAIL_HOST_PASSWORD = 'nhxiigqwinlehgmi'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
