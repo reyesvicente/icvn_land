0ee291b201df94fd26b0573ef3aa1203953b41cd
Vicente Antonio G. Reyes <highcenoid@Highcenburg.local>
2020-04-04 01:53:25 +0800

    sign check

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret
 SECRET_KEY = '-d__*#1#c%_duglxr+a_!e5b@t4(7k3xoox@r+w$hj8^mdcgqf'
 
 # SECURITY WARNING: don't run with debug turned on in production
